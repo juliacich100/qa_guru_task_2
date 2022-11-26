89a783f01f021240655e2e102c1fc882fca9d336
iuliiach <h46@yandex.ru>
2022-11-26 15:39:35 +0300

    trained running tests

---
+++ simple_test.py
@@ -0,0 +1,19 @@
+
+def test_something():
+    assert 1 == 2, "Мы проверяем это для теста, оно должно падать"
+
+
+def test_something_one():
+    pass
+
+
+def test_something2():
+    pass
+
+
+def test_something3():
+    pass
+
+
+def test_something4():
+    pass
